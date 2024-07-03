e7e20344d1e16f446a60226795e5d7f9fd2a1aaa
Beatrice Ambos <beatrice.ambos@web.de>
2024-07-03 16:17:25 +0200

    finish functionality

--- main.go
+++ main.go
@@ -8,7 +8,19 @@ func main() {
 	fmt.Println("Hello World!")
 }
 
-func FizzBuzz() string {
-	return ""
-}
+func FizzBuzz(number int) string {
 
+	if number%15 == 0 {
+		return "FizzBuzz"
+	}
+
+	if number%5 == 0 {
+		return "Buzz"
+	}
+
+	if number%3 == 0 {
+		return "Fizz"
+	}
+
+	return fmt.Sprint(number)
+}
